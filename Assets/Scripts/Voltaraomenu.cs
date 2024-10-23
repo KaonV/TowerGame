097f0447971ee0cc37ab7fb8b0cbabf8d3a58095
Alexia <ab.dantas@hotmail.com>
2024-10-23 15:26:48 -0300

    Menu principal funcionando
    adição das artes e programação do menu principal, adição das cenas de criar sala, entrar em sala, créditos e opções e adição de um botão de voltar em todas as cenas.

---
+++ Assets/Scripts/Voltaraomenu.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Voltaraomenu : MonoBehaviour
+{
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
